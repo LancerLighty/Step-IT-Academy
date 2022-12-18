54de2718f1d207080e4b910ce684cb88b9e95aa7
TekLighty <111767700+TekLighty@users.noreply.github.com>
2022-12-18 16:29:55 +0400

    multiplications with javascript(+-/*)

---
+++ multiplication-javascript/script.js
@@ -0,0 +1,6 @@
+var firstnum = Number(prompt());
+var secondnum = Number(prompt());
+console.log(firstnum + ' + ' + secondnum + ' = ' + (firstnum+secondnum));
+console.log(firstnum + ' - ' + secondnum + ' = ' + (firstnum-secondnum));
+console.log(firstnum + ' * ' + secondnum, ' = ' + firstnum*secondnum);
+console.log(firstnum + ' / ' + secondnum, ' = ' + firstnum/secondnum);
