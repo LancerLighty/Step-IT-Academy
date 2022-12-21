fabbca3586c9cf52b62cde51e17a385ffde54a30
TekLighty <111767700+TekLighty@users.noreply.github.com>
2022-12-21 17:57:33 +0400

    multiplication function

--- multiplication-javascript/script.js
+++ multiplication-javascript/script.js
@@ -1,6 +1,29 @@
+// var firstnum = Number(prompt());
+// var secondnum = Number(prompt());
+// console.log(firstnum + ' + ' + secondnum + ' = ' + (firstnum+secondnum));
+// console.log(firstnum + ' - ' + secondnum + ' = ' + (firstnum-secondnum));
+// console.log(firstnum + ' * ' + secondnum, ' = ' + firstnum*secondnum);
+// console.log(firstnum + ' / ' + secondnum, ' = ' + firstnum/secondnum)
+
+function add(a,b){
+    console.log(a + ' + ' + b + ' = ' + (a+b));
+}
+function sub(a,b){
+    console.log(a + ' - ' + b + ' = ' + (a-b));
+}
+function mul(a,b){
+    console.log(a + ' * ' + b + ' = ' + a*b);
+}
+function div(a,b){
+    console.log(a + ' / ' + b + ' = ' + a/b);
+}
+function everything(a,b){
+    add(a,b);
+    sub(a,b);
+    mul(a,b);
+    div(a,b);
+}
 var firstnum = Number(prompt());
 var secondnum = Number(prompt());
-console.log(firstnum + ' + ' + secondnum + ' = ' + (firstnum+secondnum));
-console.log(firstnum + ' - ' + secondnum + ' = ' + (firstnum-secondnum));
-console.log(firstnum + ' * ' + secondnum, ' = ' + firstnum*secondnum);
-console.log(firstnum + ' / ' + secondnum, ' = ' + firstnum/secondnum);
+console.log(firstnum, secondnum);
+everything(firstnum,  secondnum);
