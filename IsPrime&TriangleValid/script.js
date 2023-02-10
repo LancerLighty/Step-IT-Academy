92173c191ac00c3b6bddc7a528301547b441855b
TekLighty <111767700+TekLighty@users.noreply.github.com>
2023-02-10 07:14:34 +0400

    Is Prime And Check If Triangle Is Valid

---
+++ IsPrime&TriangleValid/script.js
@@ -0,0 +1,30 @@
+// function perimeter(a,b,c){
+//     if((a+b>c) && (a+c>b) && (b+c>a) && a>0 && b>0 && c>0){
+//         return a+b+c;
+//     }
+// }
+// var perimeter1 = document.querySelector(".perimeter");
+// var first = Number(prompt());
+// var second = Number(prompt());
+// var third = Number(prompt());
+// perimeter1.innerText = perimeter(first, second, third);
+// var arr = [5, 10, 2, 6, 3];
+// check(arr);
+// function check(arr){
+//     for(var i of arr){
+//         var isPrime = true;
+//         for(var y = 2; y <= i/2; i++){
+//             if(i == 0){
+//                 console.log( i + " arc martivia da arc shedgenili");
+//             }else if(i % y == 0){
+//                 isPrime = false;
+//                 break;
+//             }
+//         }
+//         if(isPrime){ 
+//             console.log(i + " ricxvi martivia");
+//         } else {
+//             console.log(i + " ricxvi shedgenilia");
+//         }
+//     }
+//}
