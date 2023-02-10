0f8b11db591d5603337a1b63222869c967e447d9
TekLighty <111767700+TekLighty@users.noreply.github.com>
2023-02-10 07:07:02 +0400

    same word

--- samewordsinprompt/script.js
+++ samewordsinprompt/script.js
@@ -3,11 +3,11 @@ var pr2 = String(prompt());
 var p =document.querySelector(".similar");
 if(pr1 == pr2){
     p.innerText = "ori shemotanili sityva identuria";
-    p.style.color = "rgb(0, 189, 102)"
+    p.style.color = "rgb(0, 189, 102)"; //green
 } else if(pr1.toLowerCase() == pr2.toLowerCase()){
     p.innerText = "ori shemotanili sityva nawilobriv identuria";
-    p.style.color = "blue"
+    p.style.color = "blue";
 } else {
     p.innerText = "ori shemotanili sityva ar aris identuri";
-    p.style.color = "red"
+    p.style.color = "red";
 }
